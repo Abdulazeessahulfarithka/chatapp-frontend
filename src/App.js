b1bd3920f13a7147c05b48826574845c2ecfba69
“Abdul <kaazees31@gmail.com>
2024-01-25 12:38:29 +0530

    NEW

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import './App.css';
-import MainContainer from './Components/MainContainer';
+import React from "react";
+
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
-    
-    <MainContainer/>  
+      <NavBar></NavBar>
     </div>
   );
 }
